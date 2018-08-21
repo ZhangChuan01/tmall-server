03b6c4f7fcaa1a7c4e891e4ba6467c215822b84a
ZhangChuan01 <993090861@qq.com>
2018-08-21 19:40:05 +0800

    Update SessionInterceptor.java

--- src/main/java/com/tmall/tmallend/filter/SessionInterceptor.java
+++ src/main/java/com/tmall/tmallend/filter/SessionInterceptor.java
@@ -13,7 +13,7 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler) throws Exception{
         response.setContentType("text/html;charset=UTF-8");
-        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
